49cb8706f7769de683e61b289516adb5e1fde937
Бакдаулет Аблаев <bakdaulet@Air-Bakdaulet.bank.corp.centercredit.kz>
2022-11-28 10:58:05 +0600

    Initial commit

---
+++ src/test/java/bit/bakdaulet/java/backend/EmployeeDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package bit.bakdaulet.java.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
